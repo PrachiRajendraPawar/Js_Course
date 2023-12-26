da1e0df856ecda913af41abc309e6774f961da4e
Prachi Pawar <147933789+PrachiRajendraPawar@users.noreply.github.com>
2023-12-26 07:01:19 +0000

    variables in javascripts

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const account_id=1230
+let account_Email="prp11@gamil.com"
+var account_password=123456
+account_city="Pune"
+let account_status;
+// account_id=1  //not allow
+console.log(account_id);
+console.table([account_id,account_Email,account_password,account_city,account_status]);
