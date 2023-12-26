74269a62e86aed31b8ccace8325ce376b68d17d5
Prachi Pawar <147933789+PrachiRajendraPawar@users.noreply.github.com>
2023-12-26 07:53:03 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+let age=21;
+let name="Prachi";
+let Isregular=true;
+
+console.log(typeof "prachi");
+console.log(typeof age);
+console.log(typeof Isregular);
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
