2d7f2003e9aafec2a59f221c81b427904e9a9280
Prachi Pawar <147933789+PrachiRajendraPawar@users.noreply.github.com>
2023-12-26 08:34:00 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,51 @@
+// let score="90abc"
+
+// console.log(score)
+// console.log(typeof score);
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// let age=null
+// let valueInNumber1=Number(age)
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1)
+
+// let un=undefined
+// let valueInNumber2=Number(un)
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2)
+
+// let boolenvalue=true
+// let valueInNumber3=Number(boolenvalue)
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3)
+
+// let strvalue="prachi"
+// let valueInNumber4=Number(strvalue)
+// console.log(typeof valueInNumber4);
+// console.log(valueInNumber4)
+
+// conversion number to boolen
+
+// let num=1;//true
+// let num2=0;//false
+// let num3="";//false
+// let num4="prachi";//true
+// let boolenInvalue=Boolean(num4);
+// console.log(boolenInvalue);
+
+//conversion int to string
+
+let somenumber=90;
+
+let stringnumber=String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+let std=90.809997869;
+
+let numvalue=Number(std)
+console.log(numvalue);
+console.log(typeof numvalue);
