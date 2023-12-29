ad4077479c95d8bd24c3b8d99448671bdd4cc1fd
Prachi Pawar <147933789+PrachiRajendraPawar@users.noreply.github.com>
2023-12-29 06:31:25 +0000

    conversion and comparison

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -38,14 +38,47 @@
 
 //conversion int to string
 
-let somenumber=90;
+// let somenumber=90;
 
-let stringnumber=String(somenumber);
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// let stringnumber=String(somenumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
 
-let std=90.809997869;
+// let std=90.809997869;
 
-let numvalue=Number(std)
-console.log(numvalue);
-console.log(typeof numvalue);
+// let numvalue=Number(std)
+// console.log(numvalue);
+// console.log(typeof numvalue);
+
+//********************************Operations*****************************
+
+// let value=3
+// let negativeValue=-value
+// console.log(negativeValue)
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(9**2)
+// console.log(90%7)
+// console.log(8-7)
+
+// let str1="Hello"
+// let str2="  Prachi"
+// let str3= str1+str2;
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(2+3+"3")
+
+// console.log((3*9)+(54/6))
+
+// console.log(+true)
+// console.log(+"")
+
+let counter=100;
+counter++;
+++counter;
+console.log(counter)
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2<9)
+// console.log(2>9);
+// console.log(2==9)
+// console.log(2!=9)
+// console.log(2>=2)
+// console.log(4<=4)
+
+//notes: the reson is that an euality check == and comparisins >,<,>=,<= work differently.
+//comparison is convert null to a number,treating it as 0.
+// that's why (3) null>=0 is true and (1)null>0 is false.
+
+// console.log("2">0)
+// console.log("2"<0)
+
+// console.log(null>0)
+// console.log(null>=0)
+// console.log(null==0)
+// console.log("******************************************")
+// console.log(undefined>0)
+// console.log(undefined>=0)
+// console.log(undefined==0)
+// console.log(undefined<0)
+// console.log(undefined<=0)
+// console.log(undefined!=0)
+
+//strict check
+
+console.log("2"==="2")
